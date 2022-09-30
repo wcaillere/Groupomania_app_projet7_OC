512d8cf8026c615c76b93edf8c1bd0699f333b7d
William Caillere <wcaillere.pro@gmail.com>
2022-09-30 12:02:15 +0200

    Mise en place de la connexion à la MySQL DB

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 //import packages
 const app = require('./app');
+const mysql = require('mysql');
 require('dotenv').config();
 
 //return a port. Val can be a string or a Number
@@ -14,9 +15,35 @@ const normalizePort = val => {
     }
     return false;
   };
-  const port = normalizePort(process.env.PORT || '3000');
+  const port = normalizePort(process.env.API_PORT || '3000');
   app.set('port', port);
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
-})
+});
+
+//Create the connexion to MySQL DB
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+});
+
+//Establish the connexion
+connection.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+    console.log('connected as id ' + connection.threadId);
+});
+
+// connection.end(function(err) {
+//     if (err) {
+//         console.log(err);
+//         return;
+//     }
+//     console.log('connexion endend');
+// });
