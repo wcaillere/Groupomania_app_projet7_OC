062ad9d78af71d4c8464acc9fa539bb762a8dbdf
William Caillere <wcaillere.pro@gmail.com>
2022-09-30 15:10:53 +0200

    test connexion DB dans un controller

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,13 @@
+//Imports DB's connection
+const connection = require('../db')
+
 //Allows an user to signup on the site if he's not
 exports.signup = (req, res, next) => {
-    console.log('hello')
-    res.status(500).json({message: 'test'})
+    connection.query('CREATE TABLE test(id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY)', function (error, results, fields) {
+        if (error) {
+            res.status(500).json({error})
+        } else {
+            res.status(200).json({message: 'table créée'})
+        }
+    });
 }
--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 //imports packages
 const app = require('./app');
-const mysql = require('mysql');
 require('dotenv').config();
 
 //returns a port. Val can be a string or a Number
@@ -17,32 +16,6 @@ const normalizePort = val => {
   };
   const port = normalizePort(process.env.API_PORT || '3000');
   app.set('port', port);
-
-//Creates the connection to MySQL DB
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-});
-
-//Establishes the connection
-connection.connect(function(err) {
-    if (err) {
-      console.error('error connecting: ' + err.stack);
-      return;
-    }
-    console.log('connected as id ' + connection.threadId);
-});
-
-// connection.end(function(err) {
-//     if (err) {
-//         console.log(err);
-//         return;
-//     }
-//     console.log('connection endend');
-// });
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
