f9e99869f872229249a5d3c6650eeb401a49ca0c
William Caillere <wcaillere.pro@gmail.com>
2022-09-30 11:08:31 +0200

    Configuration of api with cors and helmet

--- server/app.js
+++ server/app.js
@@ -1,6 +1,12 @@
 //import packages
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
 
+//Create and configure express app
 const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(helmet())
 
 module.exports = app;
