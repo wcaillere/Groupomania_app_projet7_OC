22b9a650d253ba808a52234b478db99867e10de5
William Caillere <wcaillere.pro@gmail.com>
2022-10-04 15:47:34 +0200

    Ajout config prettier

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -4,34 +4,33 @@ import styled from 'styled-components'
 import colors from '../../utils/style/colors'
 
 const HeaderLogo = styled.img`
-    height: 40px;
+  height: 40px;
 `
 
 const NavContainer = styled.nav`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 20px 0px 20px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px 0px px 20px;
 `
 
 const NavItem = styled(Link)`
-    color: ${colors.Tertiary};
-    text-decoration: none;
-    padding-right: 30px;
-    font-weight: bold;
+  color: ${colors.Tertiary};
+  text-decoration: none;
+  padding-right: 30px;
+  font-weight: bold;
 `
 
-
 function Header() {
-    return (
-        <NavContainer>
-            <HeaderLogo src= { Logo } />
-            <div>
-                <NavItem to="/login">Se connecter</NavItem>
-                <NavItem to="/signup">S'inscrire</NavItem>
-            </div>
-        </NavContainer>
-    )
+  return (
+    <NavContainer>
+      <HeaderLogo src={Logo} />
+      <div>
+        <NavItem to="/login">Se connecter</NavItem>
+        <NavItem to="/signup">S'inscrire</NavItem>
+      </div>
+    </NavContainer>
+  )
 }
 
-export default Header;
+export default Header
