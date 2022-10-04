f328ce0d9fa19eb1300fe131b346ba52fba1b5fa
William Caillere <wcaillere.pro@gmail.com>
2022-10-04 15:41:34 +0200

    Ajout d'une zone de connexion dans la page d'accueil (login)

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,9 +1,11 @@
 import Header from "../../components/Header";
+import Connection from "../../components/connection";
 
 function Home() {
     return (
         <div>
             <Header />
+            <Connection />
         </div>
     )
 }
