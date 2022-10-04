b92008dc0434a6a2a743265173ab0ea235875026
William Caillere <wcaillere.pro@gmail.com>
2022-10-04 11:30:49 +0200

    Mise en place frontend avec create react app + react-router-dom

---
+++ client/src/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from '../src/pages/Home'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
---
+++ client/src/pages/Home/index.jsx
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <div>
+            <h1>Page d'accueil</h1>
+        </div>
+    )
+}
+
+export default Home;
