23cf668c41ead2b7978d848077d0e165c91c4b70
William Caillere <wcaillere.pro@gmail.com>
2022-10-05 16:26:36 +0200

    Ajout css page de connexion

--- client/src/components/connection/index.jsx
+++ client/src/components/connection/index.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 import colors from '../../utils/style/colors';
 import login_img from '../../assets/login_img.jpg';
 
@@ -17,7 +18,9 @@ const ConnectionLeft = styled.div`
 `;
 
 const ConnectionRight = styled.div`
+  position: relative;
   width: 50%;
+  padding: 60px;
   background: ${colors.secondary};
 `;
 
@@ -28,13 +31,81 @@ const LoginImg = styled.img`
   opacity: 60%;
 `;
 
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const TitleInput = styled.label`
+  font-size: 30px;
+  font-weight: bold;
+  margin-top: 30px;
+`;
+
+const FormInput = styled.input`
+  display: block;
+  width: 100%;
+  margin-top: 20px;
+  border: none;
+  border-radius: 15px;
+  font-size: 20px;
+  padding: 16px;
+  ::placeholder {
+    color: ${colors.Tertiary};
+    font-weight: bold;
+    opacity: 80%;
+  }
+`;
+
+const ConnectButton = styled.input`
+  width: 100%;
+  font-size: 36px;
+  margin: 70px auto 20px auto;
+  padding: 25px;
+  border: none;
+  border-radius: 15px;
+  color: white;
+  background: ${colors.primary};
+  box-shadow: 2px 3px 5px 0px #9c9c9c;
+`;
+
+const SignupLink = styled(Link)`
+  position: absolute;
+  bottom: 20px;
+  right: 20px;
+  color: ${colors.Tertiary};
+`;
+
 function Connection() {
   return (
     <ConnectionContainer>
       <ConnectionLeft>
         <LoginImg src={login_img} alt="login" />
       </ConnectionLeft>
-      <ConnectionRight></ConnectionRight>
+      <ConnectionRight>
+        <Form>
+          <TitleInput>
+            Adresse mail
+            <FormInput
+              type="text"
+              name="email"
+              placeholder="exemple@mail.com"
+            />
+          </TitleInput>
+          <TitleInput>
+            Mot de passe
+            <FormInput
+              type="text"
+              name="password"
+              placeholder="Entrez votre mot de passe"
+            />
+          </TitleInput>
+          <ConnectButton type="submit" value="Se connecter" />
+        </Form>
+        <SignupLink to="/signup">
+          Pas encore de compte ? Inscrivez-vous ici !
+        </SignupLink>
+      </ConnectionRight>
     </ConnectionContainer>
   );
 }
