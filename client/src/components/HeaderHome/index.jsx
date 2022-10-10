26824e905c35609ef4859231042d6d5cbe7bcc29
William Caillere <wcaillere.pro@gmail.com>
2022-10-10 12:21:20 +0200

    Ajout font awesome dans html + commencement header homepage

---
+++ client/src/components/HeaderHome/index.jsx
@@ -0,0 +1,34 @@
+import Logo from '../../assets/logo_white.svg';
+import styled from 'styled-components';
+import colors from '../../utils/style/colors';
+
+const HeaderLogo = styled.img`
+  height: 30px;
+`;
+
+const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+  background: ${colors.Tertiary};
+`;
+
+function HeaderHome() {
+  return (
+    <HeaderContainer>
+      <HeaderLogo src={Logo} alt="Logo Groupomania" />
+      <i
+        class="fa-solid fa-power-off"
+        style={{
+          color: 'white',
+          height: '30px',
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      ></i>
+    </HeaderContainer>
+  );
+}
+
+export default HeaderHome;
--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,5 +1,17 @@
+import styled from 'styled-components';
+import HeaderHome from '../../components/HeaderHome';
+
+const MainContainer = styled.main`
+  background: #e7e7e7;
+`;
+
 function Home() {
-  return <div>test</div>;
+  return (
+    <div>
+      <HeaderHome />
+      <MainContainer>test</MainContainer>
+    </div>
+  );
 }
 
 export default Home;
