c2c8d6af69deb03ee519143e5bdb977eb1d663d0
William Caillere <wcaillere.pro@gmail.com>
2022-10-10 15:56:19 +0200

    Commencement CSS composant react "Post"

---
+++ client/src/components/post/index.jsx
@@ -0,0 +1,92 @@
+import styled from 'styled-components';
+import colors from '../../utils/style/colors';
+
+const PostContainer = styled.div`
+  position: relative;
+  width: 90%;
+  background: white;
+  margin: 20px auto;
+  padding: 12px;
+  border-radius: 10px;
+`;
+
+const PostDescrition = styled.div`
+  display: flex;
+  margin-bottom: 15px;
+`;
+
+const Postinitial = styled.div`
+  display: flex;
+  background: ${colors.secondary};
+  width: 40px;
+  height: 40px;
+  margin-right: 10px;
+  border-radius: 50px;
+  font-size: 20px;
+  font-weight: bold;
+  align-items: center;
+  justify-content: center;
+`;
+
+const PostAuthor = styled.div`
+  font-weight: bold;
+  font-size: 18px;
+`;
+
+const PostDate = styled.div`
+  font-size: 13px;
+`;
+
+const PostContent = styled.div`
+  font-size: 15px;
+  text-align: justify;
+`;
+
+const PostLike = styled.div`
+  display: flex;
+  position: absolute;
+  top: 12px;
+  right: 12px;
+`;
+
+const PostLikeIcon = styled.div`
+  margin-left: 5px;
+`;
+
+const PostImage = styled.img``;
+
+function Post() {
+  return (
+    <PostContainer>
+      <PostDescrition>
+        <Postinitial>F</Postinitial>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+          }}
+        >
+          <PostAuthor>Fred Doe</PostAuthor>
+          <PostDate>15/04/2012</PostDate>
+        </div>
+      </PostDescrition>
+      <PostContent>
+        Lorem ipsum dolor sit amet,
+        <br /> consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua.
+        <br /> Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex.
+      </PostContent>
+      <PostLike>
+        8
+        <PostLikeIcon>
+          <i class="fa-solid fa-thumbs-up"></i>
+        </PostLikeIcon>
+      </PostLike>
+      <PostImage></PostImage>
+    </PostContainer>
+  );
+}
+
+export default Post;
--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,16 +1,21 @@
 import styled from 'styled-components';
 import HeaderHome from '../../components/HeaderHome';
 import FooterHome from '../../components/FooterHome';
+import Post from '../../components/post';
 
 const MainContainer = styled.main`
   background: #e7e7e7;
+  padding: 20px 0px;
 `;
 
 function Home() {
   return (
     <div>
       <HeaderHome />
-      <MainContainer>test</MainContainer>
+      <MainContainer>
+        <Post />
+        <Post />
+      </MainContainer>
       <FooterHome />
     </div>
   );
