d6e7dcaf68ad29023812d935780a29c9c73f00dd
William Caillere <wcaillere.pro@gmail.com>
2022-10-24 15:51:00 +0200

    Ajout darktheme pour page 404

--- client/src/pages/ErrorPage/index.jsx
+++ client/src/pages/ErrorPage/index.jsx
@@ -1,13 +1,28 @@
-import Logo from '../../assets/logo_red.svg';
+import { useContext } from 'react';
+import { ThemeContext } from '../../utils/context/index';
+import LogoRed from '../../assets/logo_red.svg';
+import LogoWhite from '../../assets/logo_white.svg';
 import './errorPage.css';
 
 //Returns the page for Error 404
 function ErrorPage() {
+  const theme = useContext(ThemeContext).theme;
+
   return (
-    <div className="errorContainer">
-      <img src={Logo} alt="Logo groupomania" className="errorLogo" />
-      <span className="error404">404</span>
-      <span className="errorTxt">
+    <div
+      className={
+        'errorContainer ' + (theme === 'dark' ? 'errorContainerDark' : '')
+      }
+    >
+      <img
+        src={theme === 'dark' ? LogoWhite : LogoRed}
+        alt="Logo groupomania"
+        className="errorLogo"
+      />
+      <span className={'error404 ' + (theme === 'dark' ? 'darkText' : '')}>
+        404
+      </span>
+      <span className={'errorText ' + (theme === 'dark' ? 'darkText' : '')}>
         Oops ! La page que vous recherchez semble introuvable !
       </span>
     </div>
