338a15e4ab9ae68d6de1b316c2afd4335b8cec77
William Caillere <wcaillere.pro@gmail.com>
2022-10-25 13:00:17 +0200

    Correction d'un bug du textarea de la popup de modif

--- client/src/components/PopupPost/index.jsx
+++ client/src/components/PopupPost/index.jsx
@@ -1,5 +1,4 @@
-import test from '../../assets/test.jpg';
-import { useContext, useState, useRef, useEffect } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import { ThemeContext } from '../../utils/context/index';
 import '../post/post.css';
 import './popupPost.css';
@@ -7,29 +6,26 @@ import './popupPost.css';
 //Returns The Post as a popup when the user wants to modify it
 function PopupPost(props) {
   const theme = useContext(ThemeContext).theme;
-  const [postData, setPostData] = useState('');
-  const firstUpdate = useRef(true);
+  const [postData, setPostData] = useState({});
+  const [content, setContent] = useState('');
 
   useEffect(() => {
-    if (firstUpdate.current) {
-      firstUpdate.current = false;
-    } else {
-      fetch(`http://localhost:5000/api/posts/${props.idPost}`, {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem('token')}`,
+    fetch(`http://localhost:5000/api/posts/${props.idPost}`, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+    })
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          console.log(result);
+          setPostData(result[0]);
+          setContent(result[0].content);
         },
-      })
-        .then((res) => res.json())
-        .then(
-          (result) => {
-            console.log(result[0]);
-            setPostData(result[0]);
-          },
-          (error) => {
-            console.log(error);
-          }
-        );
-    }
+        (error) => {
+          console.log(error);
+        }
+      );
   }, [props.idPost]);
 
   return props.trigger ? (
@@ -42,7 +38,7 @@ function PopupPost(props) {
       >
         <div className="postHeader">
           <div className="postDescrition">
-            <div className="postinitial">{postData.firstname[0]}</div>
+            <div className="postinitial">F</div>
             <div
               className={
                 'postDetails ' + (theme === 'dark' ? 'postDetailsDark' : '')
@@ -55,7 +51,7 @@ function PopupPost(props) {
               >
                 {`${postData.firstname} ${postData.lastname[0]}.`}
               </div>
-              <div className="postDate">15/04/2012</div>
+              <div className="postDate">{postData.date}</div>
             </div>
           </div>
         </div>
@@ -69,11 +65,23 @@ function PopupPost(props) {
           <i className="fa-solid fa-xmark fa-2xl"></i>
         </button>
         <textarea
+          id="PopupText"
+          name="PopupText"
           rows={3}
+          required
           className="popupContent"
-          defaultValue={postData.content}
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
         ></textarea>
-        <img className="postImage" src={test} alt="illustration du post"></img>
+        {postData.image_url === null ? (
+          ''
+        ) : (
+          <img
+            className="postImage"
+            src={postData.image_url}
+            alt="illustration du post"
+          />
+        )}
       </div>
     </div>
   ) : (
